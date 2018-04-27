7caa5c0b48a9581c2e230c9646236614bea34eca
Jan Horbowicz <janhor.astro@gmail.com>
2018-04-27 21:58:17 +0200

    add new function

--- js_ex/functions.js
+++ js_ex/functions.js
@@ -25,14 +25,48 @@ function kebabToSnake(kebabString){
     return kebabString.replace(/-/g, "_");
 }
 
-//expect true
-console.log(isEven(2));
-//expect false
-console.log(isEven(111));
-//expect 1
-console.log(factorial(0));
-//expect 7!
-console.log(factorial(7));
-console.log(1*2*3*4*5*6*7);
-//expect i_love_kebab
-console.log(kebabToSnake("i-love-kebab"));
+function printReversed(list) {
+    var maxIndex = list.length - 1;
+    list.forEach(function(element){
+        var index = list.indexOf(element);
+        console.log(list[maxIndex-index]);
+    })
+}
+
+function isUniform(list) {
+    var buff = list[0];
+    for (var el in list) {
+        console.log (list[el], buff)
+        if(list[el] !== buff) {
+            return false;
+        } else {
+            buff = list[el];
+        }
+    }
+    return true;
+}
+
+
+//          isUniform test
+// console.log(isUniform([1,1,1,1]));
+// console.log(isUniform([1,2,1,1]));
+// console.log(isUniform(["a","a","a"]));
+// console.log(isUniform(["a","b","b","b"]));
+
+//!!        printReversed test
+// printReversed([1,2,3,4,5,6]);
+// printReversed(["one","two","three"]);
+
+
+
+// //expect true
+// console.log(isEven(2));
+// //expect false
+// console.log(isEven(111));
+// //expect 1
+// console.log(factorial(0));
+// //expect 7!
+// console.log(factorial(7));
+// console.log(1*2*3*4*5*6*7);
+// //expect i_love_kebab
+// console.log(kebabToSnake("i-love-kebab"));
