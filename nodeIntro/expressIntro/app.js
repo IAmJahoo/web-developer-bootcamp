7da1a4633c4a99441c63e2e25c19aca717dfb885
IAmJahoo <janhor.astro@gmail.com>
2018-08-07 23:15:17 +0200

    simple server

--- nodeIntro/expressIntro/app.js
+++ nodeIntro/expressIntro/app.js
@@ -1,8 +1,20 @@
 let express = require('express');
 let app = express();
 
+let listenPort = 3000;
+
 app.get('/', (req, res) => {
     res.send('Kasia jest fajna i w ogóle!');
 });
 
-app.listen(17000);
+app.get('/bye', (req, res) => {
+    res.send('Kasia jest fajna i w ogóle ale teraz mówi papa :(');
+});
+
+app.get('*', (req, res) => {
+    res.send('Nie ma takiego routa jak ' +req.path.toString() + ' :O')
+})
+
+app.listen(listenPort, () => {
+    console.log('Server running on port ' + listenPort)
+});
